c1b6ad3456f6565e9ebc5161331c095d67f80107
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-04-03 14:37:24 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        jakarta:['Plus Jakarta Sans'],
+        DMSerif:['DM Serif Display'],
+      },
+    },
+  },
+  plugins: [],
+}
